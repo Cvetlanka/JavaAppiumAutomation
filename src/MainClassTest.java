a3c19f3a23da63752966417300416a7779b7d10b
Svetlana <write_here@bk.ru>
2025-04-30 19:06:50 +0300

    Homework 1.2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,16 +2,26 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass{
     @Test
-    public void testGetLocalNumber(){
+    public void testGetLocalNumber() {
         int trueResult = 14;
 
         int num = this.getLocalNumber();
         if (num != trueResult) {
-            System.out.println("Упс.. Вернулось неверное значение =" + num + " :(");
+            System.out.println("Упс..:( Вернулось неверное значение =" + num);
+        } else {
+            System.out.println("Ура! :) Значение верное =" + num);
         }
-        else{
-            System.out.println("Ура! Значение верное =" + num + " :)");
-        }
+    }
+    @Test
+    public void testGetClassNumber(){
+            int trueResult = 45;
 
+            int num = this.getClassNumber();
+            if (num > trueResult) {
+                System.out.println("Ура! :) Значение верное = " + num + " (>" + trueResult + ")");
+            }
+            else {
+                System.out.println("Упс..:( Вернулось неверное значение = " + num + " (<=" + trueResult + ")");
+            }
     }
 }
